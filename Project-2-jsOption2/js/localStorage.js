42b9d82604d161124c1aaf7ff26271c76763f77a
Ignacio Cl <juanigcl2532@gmail.com>
2021-06-30 14:07:15 -0600

    Agrego cambios en los estilos y se agrega modal

--- Project-2-jsOption2/js/localStorage.js
+++ Project-2-jsOption2/js/localStorage.js
@@ -1,6 +1,6 @@
 import Task from './task.js';
 function getFromLocalStorage() {
-const SavedTasks = JSON.parse(localStorage.getItem('Tasks')); 
+const SavedTasks = JSON.parse(localStorage.getItem('Tasks')); // Parse en JSON hace un array a un objeto 
 if (SavedTasks) {
     for (let i = 0; i < SavedTasks.length; i++) {
     const LocalTask = new Task(SavedTasks[i].input, SavedTasks[i].categorie, SavedTasks[i].UniqueId,SavedTasks[i].status);
--- Project-2-jsOption2/js/util.js
+++ Project-2-jsOption2/js/util.js
@@ -1,6 +1,6 @@
 
 function ChangeNameTask(e) {
-    const editInput = e.querySelector('input[type=text]');
+    const editInput = e.querySelector('input[type=text]');// Se puede hacer un selector como queryselector desde un elemento especifico, Descubrimiento nuevo: se puede llamar a un elemento especifico usando el li en este caso en vez de document asi se es mas especifico 
     const label = e.querySelector('label');
     const TaskCreated = e.classList.contains('editMode');
     if (TaskCreated) {
@@ -12,39 +12,45 @@ function ChangeNameTask(e) {
 }   
 
 function editTask(buttonEdit, li) {
-    buttonEdit.addEventListener('click', (e) => {
-        const especificTask = li;
-        if (e.target.tagName === 'IMG') {
-            if (e.target.parentElement.classList.contains('edit')) {
-                ChangeNameTask(especificTask);
-            }
+buttonEdit.addEventListener('click', (e) => {
+    const especificTask = li;
+    if (e.target.tagName === 'IMG') {
+        if (e.target.parentElement.classList.contains('edit')) {
+            ChangeNameTask(especificTask);
         }
-    });
+    }
+});
 }
-
 function deleteTask(buttonDelete, li) {
-    buttonDelete.addEventListener('click', (e) => {
-        if (e.target.tagName === 'IMG') {
-            if (e.target.parentElement.classList.contains('delete')) {
-                li.remove();
-            }
+const closeModal = document.querySelector('.button-remove');
+const modalContainer = document.querySelector('.modal-container');
+buttonDelete.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        if (e.target.parentElement.classList.contains('delete')) {
+            li.remove();
+            modalContainer.classList.add('show');
         }
-    });
+    }
+});
+
+
+closeModal.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
+});
 }
-
-
 
 function deleteAllTasks() {
-    const deleteAll = document.querySelector('.deleteAll h4');
-    deleteAll.addEventListener('click', () => {
-        const allTasks = document.querySelectorAll('#taskul input[type=checkbox]');
-        for (let i = 0; i < allTasks.length; i++) {
+const deleteAll = document.querySelector('.deleteAll h4');
+deleteAll.addEventListener('click', () => {
+    const allTasks = document.querySelectorAll('#taskul input[type=checkbox]');
+    for (let i = 0; i < allTasks.length; i++) {
         if (allTasks[i].checked === true) {
             const fatherOfTask = allTasks[i].parentElement;
             fatherOfTask.remove();
         }
-        }
-    });
+    }
+    localStorage.clear();
+});
 }
 
 export{
